f2e48696ebc5ee046a0d6e3271549c9ec5824a62
Gravemind2401 <inferi_sententia@hotmail.com>
2019-09-01 19:37:47 +1000

    initial setup

--- Studio/Properties/AssemblyInfo.cs
+++ Studio/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2e0e9419-1554-4eda-bc88-1837400f22f6")]
+[assembly: Guid("5f4c3814-8097-4869-b5e2-4c21ef45ceb2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.17")]
+[assembly: AssemblyFileVersion("1.0.0.17")]
