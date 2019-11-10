7ff971691ccf12414d22856666b959d1be91fa2d
Gravemind2401 <inferi_sententia@hotmail.com>
2019-11-10 20:32:23 +1100

    update sandbox to use MetroWindow for main window and rafted windows

--- Sandbox/Controls/RaftedWindow.xaml.cs
+++ Sandbox/Controls/RaftedWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace Sandbox.Controls
     /// <summary>
     /// Interaction logic for RaftedWindow.xaml
     /// </summary>
-    public partial class RaftedWindow : Window
+    public partial class RaftedWindow : MahApps.Metro.Controls.MetroWindow
     {
         public static readonly DependencyProperty ModelProperty =
             DependencyProperty.Register(nameof(Model), typeof(object), typeof(RaftedWindow), new PropertyMetadata((object)null));
--- Sandbox/MainWindow.xaml.cs
+++ Sandbox/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace Sandbox
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MahApps.Metro.Controls.MetroWindow
     {
         private static readonly Dictionary<string, ResourceDictionary> themes = (new[]
         {
