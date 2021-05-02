6d129bf304b9b612e656585acf817a695b9c13de
Gravemind2401 <inferi_sententia@hotmail.com>
2021-05-02 14:23:14 +1000

    expose most control styles with an explicit key so they can be referenced even when the default style is overridden

--- Studio/Properties/AssemblyInfo.cs
+++ Studio/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.71.852")]
-[assembly: AssemblyFileVersion("1.0.71.852")]
+[assembly: AssemblyVersion("1.0.72.856")]
+[assembly: AssemblyFileVersion("1.0.72.856")]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
