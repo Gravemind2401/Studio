aabfb5488791af5f5939b4d4ac825258568114a9
Gravemind2401 <inferi_sententia@hotmail.com>
2021-05-03 21:39:34 +1000

    update some more resource keys

--- Studio/Properties/AssemblyInfo.cs
+++ Studio/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.72.856")]
-[assembly: AssemblyFileVersion("1.0.72.856")]
+[assembly: AssemblyVersion("1.0.73.861")]
+[assembly: AssemblyFileVersion("1.0.73.861")]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
